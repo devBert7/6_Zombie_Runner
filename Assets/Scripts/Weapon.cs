2d2a12911ea11f9c1821d76043d9a3c6c9f7c25b
Robert Thiel <thielbert7@gmail.com>
2019-09-18 08:15:21 -0600

    Added Weapon.cs and set up Physics.Raycast(), allowing us to aim through our reticle and shoot and imaginary laser that, when encounters an object, prints to the console what the object we hit is.  We're mostly set up to cause enemy damage.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	[SerializeField] Camera FPCam;
+	[SerializeField] float range = 100f;
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			Shoot();
+		}
+	}
+
+	void Shoot() {
+		RaycastHit hit;
+
+		// (FromWhere, InWhatDirection, VariableToStoreRaycastHitPassedByReference, Range)
+		Physics.Raycast(FPCam.transform.position, FPCam.transform.forward, out hit, range);
+
+		print("We've hit a " + hit.transform.name);
+	}
+}
