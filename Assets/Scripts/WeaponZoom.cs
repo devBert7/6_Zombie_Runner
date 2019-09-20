6b022741f8111d7453af9ba48701e8463230c1be
Robert Thiel <thielbert7@gmail.com>
2019-09-20 12:28:26 -0600

    Added WeaponZoom.cs and accessed Camera to adjust field of view settings and zoom in and out with the right click of the mouse button.  Next, I adjusted the sensitivity of the camera by accessing the Rigid Body First Person Controller and adding the necessary namespace within the WeaponZoom.cs script, which allowed me to control the x and y mouse sensitivity when in zoom mode for further control.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour {
+	[SerializeField] float zoomIn = 20f;
+	[SerializeField] float zoomOut = 60f;
+	[SerializeField] float zoomInSens = .5f;
+	[SerializeField] float zoomOutSens = 2f;
+
+	bool isZoomed = false;
+	RigidbodyFirstPersonController fpsController;
+
+	void Start() {
+		fpsController = GetComponent<RigidbodyFirstPersonController>();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(1)) {
+			HandleZoom();
+		}
+	}
+
+	void HandleZoom() {
+		if (isZoomed) {
+			Camera.main.fieldOfView = zoomOut;
+			fpsController.mouseLook.XSensitivity = zoomOutSens;
+			fpsController.mouseLook.YSensitivity = zoomOutSens;
+		} else {
+			Camera.main.fieldOfView = zoomIn;
+			fpsController.mouseLook.XSensitivity = zoomInSens;
+			fpsController.mouseLook.YSensitivity = zoomInSens;
+		}
+
+		isZoomed = !isZoomed;
+	}
+}
