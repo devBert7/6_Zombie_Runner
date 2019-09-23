1ab8580a8fce594d0ea096fbd074b2f4a581fef2
Robert Thiel <thielbert7@gmail.com>
2019-09-23 09:47:34 -0600

    Created pickup parent for hierarchy which stores ammo pickup instances.  Created AmmoPickup prefab and added a child cube for a material look and removed the child's box collider while introducing a sphere collider with parent.  I also created an AmmoPickup.cs script, attached to the AmmoPickup parent object, where I set up collision functionality to match only the "Player" tagged collisions to print to the console and destroy the object.

---
+++ Assets/Scripts/Weapon/AmmoPickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour {
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("Player triggered me");
+			Destroy(gameObject);
+		}
+	}
+}
