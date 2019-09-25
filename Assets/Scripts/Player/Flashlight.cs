c07a64c0d2cb41eba4b41cd5d6e3ca5d0c6fee9a
Robert Thiel <thielbert7@gmail.com>
2019-09-25 11:04:37 -0600

    Created a flashlight attached to the players main camera and set a new Flashlight.cs script to access and decrease the flashlight angle and intensity over time, frame-rate independent (Time.deltaTime)

---
+++ Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+	[SerializeField] float lightDecay = .1f;
+	[SerializeField] float angleDecay = 1f;
+	[SerializeField] float minAngle = 40f;
+	[SerializeField] float minIntensity = 1.5f;
+
+	Light myLight;
+
+	void Start() {
+		myLight = GetComponent<Light>();
+	}
+
+	void Update() {
+		DecreaseAngle();
+		DecreaseIntensity();
+	}
+
+	void DecreaseAngle() {
+		if (myLight.spotAngle >= minAngle) {
+			myLight.spotAngle -= angleDecay * Time.deltaTime;
+		}
+	}
+
+	void DecreaseIntensity() {
+		if (myLight.intensity <= minIntensity) {
+			myLight.intensity = 0;
+		} else {
+			myLight.intensity -= lightDecay * Time.deltaTime;
+		}
+	}
+}
